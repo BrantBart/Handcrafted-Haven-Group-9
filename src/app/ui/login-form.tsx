946d565f7954e5b7b2eefea8575c38f2a418e7c4
Braxton Hotton <bhotton25@gmail.com>
2024-11-29 21:53:41 -0700

    added more updates to styling to battle contrast issues

--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -20,22 +20,22 @@ export default function LoginForm() {
     return (
       <form 
     //   action={formAction} 
-      className="space-y-3">
-        <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+      className="space-y-3 text-[#00CCC2]">
+        <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8 bg-[#0a330fff]">
           <h1 className={`mb-3 text-2xl`}>
             Please log in to continue.
           </h1>
           <div className="w-full">
             <div>
               <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                className="mb-3 mt-5 block text-xs font-medium"
                 htmlFor="email"
               >
                 Email
               </label>
               <div className="relative">
                 <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500 bg-gray-300"
                   id="email"
                   type="email"
                   name="email"
@@ -47,14 +47,14 @@ export default function LoginForm() {
             </div>
             <div className="mt-4">
               <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                className="mb-3 mt-5 block text-xs font-medium"
                 htmlFor="password"
               >
                 Password
               </label>
               <div className="relative">
                 <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500 bg-gray-300"
                   id="password"
                   type="password"
                   name="password"
@@ -66,7 +66,7 @@ export default function LoginForm() {
               </div>
             </div>
           </div>
-          <button className="mt-4 w-full" 
+          <button className="mt-4 w-full hover:bg-[#3f6634ff] p-3" 
         //   aria-disabled={isPending}
           >
             Log in 
