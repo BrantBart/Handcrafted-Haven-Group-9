ef19cfd0a7c3b4aede704de01c7859306a1d92c7
BrantBart <brant.barton64@gmail.com>
2024-12-05 22:43:32 -0700

    second fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,4 @@
-export default function LoginPage() {
+export default function AboutPage() {
   return (
     <main className="flex-grow flex flex-col items-center justify-center text-center">
       <p className="text-white">About Page</p>
--- src/app/components/footer-nav.tsx
+++ src/app/components/footer-nav.tsx
@@ -1,4 +1,3 @@
-
 export default function Footer() {
   return (
     <footer className="p-4 text-center -mx-8 -my-8">
