92a1968053d727bbdc1a88246b0eddd05909a93f
BrantBart <brant.barton64@gmail.com>
2024-12-05 22:39:26 -0700

    fix for vercel

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,3 @@
-import LoginForm from '@/app/ui/login-form';
-import Image from "next/image";
- 
 export default function LoginPage() {
   return (
     <main className="flex-grow flex flex-col items-center justify-center text-center">
