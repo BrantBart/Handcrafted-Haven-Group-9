aa894d93637b801c4d3cc270e8b73b337aef92dd
Braxton Hotton <bhotton25@gmail.com>
2024-12-10 21:56:52 -0700

    Fixed padding issues on smaller computer screens and added password validation

--- src/app/ui/create-form.tsx
+++ src/app/ui/create-form.tsx
@@ -12,6 +12,28 @@
 // import { authenticate } from '@/app/lib/actions';
 
 export default function CreateForm() {
+
+    const handleKeyUp = (): import("react").KeyboardEventHandler<HTMLInputElement> | void => {
+      const passwordInput = document.getElementById('password') as HTMLInputElement;
+      const confirmPasswordInput = document.getElementById('confirmPassword') as HTMLInputElement;
+
+      if (passwordInput && confirmPasswordInput) {
+        const password = passwordInput.value;
+        const confirmPassword = confirmPasswordInput.value;
+        if (password === confirmPassword) {
+          confirmPasswordInput.style.borderColor = 'green';
+          passwordInput.style.borderColor = 'green';
+          confirmPasswordInput.style.borderWidth = "3px";
+          passwordInput.style.borderWidth = "3px";
+        } else {
+          confirmPasswordInput.style.borderColor = 'red';
+          passwordInput.style.borderColor = 'red';
+          confirmPasswordInput.style.borderWidth = "3px";
+          passwordInput.style.borderWidth = "3px";
+        }
+      }
+    };
+
     // const [errorMessage, formAction, isPending] = useActionState(
     //   authenticate,
     //   undefined,
@@ -21,7 +43,7 @@ export default function CreateForm() {
       <form 
     //   action={formAction} text-[#00CCC2] bg-[#0a330fff]
       className="space-y-3">
-        <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8 login-form text-white">
+        <div className="flex-1 rounded-lg bg-gray-50 px-6 pt-6 login-form text-white">
           <h1 className={`mb-3 text-2xl`}>
             Create an Account with Us
           </h1>
@@ -61,6 +83,7 @@ export default function CreateForm() {
                   placeholder="Enter password"
                   required
                   minLength={6}
+                  onKeyUp={handleKeyUp}
                 />
                 {/* <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
               </div>
@@ -68,19 +91,20 @@ export default function CreateForm() {
             <div className="mt-4">
               <label
                 className="mb-3 mt-5 block text-xs font-medium"
-                htmlFor="password"
+                htmlFor="confirmPassword"
               >
                 Confirm Password
               </label>
               <div className="relative">
                 <input
                   className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-5 text-sm outline-2 placeholder:text-gray-500 bg-gray-300 text-black"
-                  id="password"
+                  id="confirmPassword"
                   type="password"
-                  name="password"
-                  placeholder="Enter password"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
                   required
                   minLength={6}
+                  onKeyUp={handleKeyUp}
                 />
                 {/* <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
               </div>
