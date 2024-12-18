55aaf7be07733fe7a4f6c4b6830e52796319c50c
Brant Barton <brant.barton64@gmail.com>
2024-12-17 23:17:19 -0700

    Update footer-nav.tsx

--- src/app/components/footer-nav.tsx
+++ src/app/components/footer-nav.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="p-4 text-center -mx-8 mt-6">
-      <p className="text-sm">© 2024 Company Name. All rights reserved.</p>
+      <p className="text-sm">© 2024 Handcrafted Haven. All rights reserved.</p>
     </footer>
   );
 }
